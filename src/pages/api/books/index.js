d2b0af738e484509fd65eea8c46423834bb9ccb4
ib-rim <ib.bashir@hotmail.com>
2023-04-23 18:34:34 +0100

    Fix variable name

--- src/pages/api/books/index.js
+++ src/pages/api/books/index.js
@@ -3,8 +3,9 @@ import Book from '../../../models/bookModel';
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            const book = await Book.find({});
-            res.status(200).json(book);
+            const books = await Book.find({});
+            console.log(books);
+            res.status(200).json(books);
         } catch (error) {
             console.log(error.message);
             res.status(500).json({ message: error.message })
