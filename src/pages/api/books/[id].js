bb6ea44fc80de8cb84a11826561bd64ab41f50ff
ib-rim <ib.bashir@hotmail.com>
2023-04-23 16:01:42 +0100

    Add handler to get individual book by id

---
+++ src/pages/api/books/[id].js
@@ -0,0 +1,20 @@
+import Book from '../../../models/bookModel';
+
+export default async function handler(req, res) {
+    const id = req.query.id;
+
+    if (req.method === 'GET') {
+        try {
+            const book = await Book.findById(id);
+            if (!book) {
+                res.status(404).json({ message: `Book with ID ${id} not found` });
+            }
+            else {
+                res.status(200).json(book);
+            }
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({ message: error.message })
+        }
+    }
+}
