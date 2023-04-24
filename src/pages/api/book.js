d96a18a226e31bd93c013ec9e2d80c92601dfa5b
ib-rim <ib.bashir@hotmail.com>
2023-04-24 10:53:23 +0100

    Add better error code for failed validation

--- src/pages/api/book.js
+++ src/pages/api/book.js
@@ -9,7 +9,12 @@ export default async function handler(req, res) {
             res.status(200).json(book);
         } catch (error) {
             console.log(error.message);
-            res.status(500).json({ message: error.message })
+            if (error.message.includes("validation failed")) {
+                res.status(400).json({ message: error.message })
+            }
+            else {
+                res.status(500).json({ message: error.message })
+            }
         }
     }
 }
