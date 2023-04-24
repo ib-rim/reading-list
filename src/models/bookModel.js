846532f39d5235115ac5452161e6071f0957e2b9
ib-rim <ib.bashir@hotmail.com>
2023-04-24 14:37:45 +0100

    Restrict status values

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -12,6 +12,7 @@ const bookSchema = mongoose.Schema(
         },
         status: {
             type: String,
+            enum: ['Unread', 'Reading', 'Read'],
             default: "Unread"
         },
         started: {
