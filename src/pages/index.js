149aa1080cb403d651a9f75f5a7260297ba1ba16
ib-rim <ib.bashir@hotmail.com>
2023-04-25 13:29:28 +0100

    Add API routes to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,101 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="p-3">
+            <h1 className="text-3xl font-bold">Reading List API</h1>
+            <p className="py-3">Return values are given in JSON.</p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className="divide-y divide-gray-600">
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-yellow-500 font-semibold mx-5">POST</span>
+                        <span>new book to the reading list</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/book</p>
+                    <table className="mx-2 table-auto border-collapse border border-gray-700">
+                        <thead>
+                            <tr className="bg-gray-950">
+                                <th className="border border-gray-700 py-1 px-2 text-left">Field</th>
+                                <th className="border border-gray-700 py-1 px-2 text-left">Type</th>
+                                <th className="border border-gray-700 py-1 px-2 text-left">Required?</th>
+                                <th className="border border-gray-700 py-1 px-2 text-left">Extra info</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="b">
+                                <td className="border border-gray-700 py-1 px-2 text-left">title</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">String</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">Yes</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">n/a</td>
+                            </tr>
+                            <tr className="">
+                                <td className="border border-gray-700 py-1 px-2 text-left">author</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">String</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">Yes</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">n/a</td>
+                            </tr>
+                            <tr className="b">
+                                <td className="border border-gray-700 py-1 px-2 text-left">status</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">String</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">No</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">Valid values are "Unread", "Reading" or "Read". Defaults to "Unread".</td>
+                            </tr>
+                            <tr className="">
+                                <td className="border border-gray-700 py-1 px-2 text-left">started</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">Date</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">No</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">ISODate format</td>
+                            </tr>
+                            <tr className="b">
+                                <td className="border border-gray-700 py-1 px-2 text-left">finished</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">Date</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">No</td>
+                                <td className="border border-gray-700 py-1 px-2 text-left">ISODate format</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-green-500 font-semibold mx-5">GET</span>
+                        <span>all books from the reading list</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/books </p>
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-green-500 font-semibold mx-5">GET</span>
+                        <span>all books from the reading list filtered using optional single or combined query parameters for title, author and status</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/books?title=[title]&author=[author]&status=[status]</p>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-green-500 font-semibold mx-5">GET</span>
+                        <span>book with given id</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/books/[id] </p>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-blue-500 font-semibold mx-5">PUT</span>
+                        <span>book with given id with new field values</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/books/[id] </p>
+                </div>
+
+                <div className="py-3">
+                    <h3 className="text-xl pb-3">
+                        <span className="text-red-500 font-semibold mx-5 2">DELETE</span>
+                        <span>book with given id from the reading list</span>
+                    </h3>
+                    <p className="text-gray-300 bg-gray-800 p-2 mx-2 mb-3">https://reading-list-eight.vercel.app/api/books/[id] </p>
+                </div>
+            </div>
+        </main>
+    )
 }
