5c625834186a8f4b1204e785a92cc61d1710f1fe
ib-rim <ib.bashir@hotmail.com>
2023-05-04 20:31:47 +0100

    Improve responsiveness and add alternating row colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from"react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
 
@@ -16,42 +16,48 @@ export default function Home() {
             })
     }
 
+    const formatDateTime = (dateTime) => {
+        if (dateTime) {
+            return dateTime.toString().split("T")[0];
+        }
+    }
+
     return (
-        <main>
-            <h1 className="text-5xl font-thin w-fit mx-20  mt-20">Reading List</h1>
-            <table className="text-black mx-20 my-5 text-left border-cyan-700 border-l-8">
+        <main> 
+            <table className="text-black lg:mx-20 my-5 text-left border-cyan-700 border-l-8">
+                <caption className="text-5xl text-white text-left font-thin my-10">Reading List</caption>
                 <thead className="text-left">
                     <tr>
-                        <th className="bg-gray-100 border-b-2 px-2 py-1">Title</th>
-                        <th className="bg-gray-100 border-b-2 px-2 py-1">Author</th>
-                        <th className="bg-gray-100 border-b-2 px-2 py-1">Status</th>
-                        <th className="bg-gray-100 border-b-2 px-2 py-1">Started</th>
-                        <th className="bg-gray-100 border-b-2 px-2 py-1">Finished</th>
+                        <th className="bg-gray-100 px-2 py-1">Title</th>
+                        <th className="bg-gray-100 px-2 py-1">Author</th>
+                        <th className="bg-gray-100 px-2 py-1">Status</th>
+                        <th className="bg-gray-100 px-2 py-1">Started</th>
+                        <th className="bg-gray-100 px-2 py-1">Finished</th>
                         <th></th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="[&>*:nth-child(odd)]:bg-gray-200 [&>*:nth-child(even)]:bg-gray-100">
                     {
                         books.map(book => {
                             return (
                                 <tr key={book._id}>
-                                    <td className="bg-gray-100 px-2 py-1">{book.title}</td>
-                                    <td className="bg-gray-100 px-2 py-1">{book.author}</td>
-                                    <td className={`bg-gray-100 px-2 py-1 font-semibold ${book.status === "Read" ? "text-green-600" : book.status === "Reading" ? "text-yellow-500" : "text-red-600" }`}>{book.status}</td>
-                                    <td className="bg-gray-100 px-2 py-1">{book.started}</td>
-                                    <td className="bg-gray-100 px-2 py-1">{book.finished}</td>
-                                    <td className="text-red-700 font-bold px-2"><button>X</button></td>
+                                    <td className="px-2 py-1">{book.title}</td>
+                                    <td className="px-2 py-1">{book.author}</td>
+                                    <td className={`px-2 py-1 font-semibold ${book.status === "Read" ? "text-green-600" : book.status === "Reading" ? "text-yellow-500" : "text-red-600"}`}>{book.status}</td>
+                                    <td className="px-2 py-1">{formatDateTime(book.started)}</td>
+                                    <td className="px-2 py-1">{formatDateTime(book.finished)}</td>
+                                    <td className="text-red-700 font-bold px-2 bg-gray-900"><button>X</button></td>
                                 </tr>
                             )
                         })
                     }
                     <tr>
-                        <td className="bg-gray-100 px-2 py-1"><input type="text" placeholder="Book Title" className="border border-gray-700 px-1"></input></td>
-                        <td className="bg-gray-100 px-2 py-1"><input type="text" placeholder="Book Author" className="border border-gray-700 px-1"></input></td>
-                        <td className="bg-gray-100 px-2 py-1"><input type="text" placeholder="Book Status" className="border border-gray-700 px-1"></input></td>
-                        <td className="bg-gray-100 px-2 py-1"><input type="text" placeholder="Started Date" className="border border-gray-700 px-1"></input></td>
-                        <td className="bg-gray-100 px-2 py-1"><input type="text" placeholder="Finished Date" className="border border-gray-700 px-1"></input></td>
-                        <td className=""><button className="text-green-500 font-bold px-2">Add book</button></td>
+                        <td className="px-2 py-1"><input type="text" placeholder="Book Title" className="border border-gray-700 px-1 w-full"></input></td>
+                        <td className="px-2 py-1"><input type="text" placeholder="Book Author" className="border border-gray-700 px-1 w-full"></input></td>
+                        <td className="px-2 py-1"><input type="text" placeholder="Book Status" className="border border-gray-700 px-1 w-full"></input></td>
+                        <td className="px-2 py-1"><input type="text" placeholder="Started Date" className="border border-gray-700 px-1 w-full"></input></td>
+                        <td className="px-2 py-1"><input type="text" placeholder="Finished Date" className="border border-gray-700 px-1 w-full"></input></td>
+                        <td className="bg-gray-900"><button className="text-green-500 font-bold px-2">Add book</button></td>
                     </tr>
                 </tbody>
             </table>
