e7b7404669e1e6ef1ca3e34ef7824f9b9252a349
ib-rim <ib.bashir@hotmail.com>
2023-05-09 15:39:22 +0100

    Added loading message

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,8 @@ export default function Home() {
     const startedError = "New book started date must be in format yyyy-mm-dd";
     const finishedError = "New book finished date must be in format yyyy-mm-dd";
 
+    const [loadingMessage, setLoadingMessage] = useState('Loading reading list ...');
+
     useEffect(() => {
         getBooks();
     }, [])
@@ -27,9 +29,12 @@ export default function Home() {
     const getBooks = async () => {
         const response = await fetch('/api/books');
         const promise = response.json();
-        promise.then((data) => {
-            setBooks(data);
-        })
+        if (response.ok) {
+            promise.then((data) => {
+                setBooks(data);
+                setLoadingMessage('');
+            })
+        }
     }
 
     const addBook = async (title, author, status, started, finished) => {
@@ -168,101 +173,105 @@ export default function Home() {
 
     return (
         <main className="lg:mx-20">
-            <table className="text-black my-5 text-left border-cyan-700 border-l-8">
-                <caption className="text-5xl text-white text-left font-thin my-10">Reading List</caption>
-                <thead className="text-left">
-                    <tr>
-                        <th className="bg-gray-100 px-2 py-1">Title</th>
-                        <th className="bg-gray-100 px-2 py-1">Author</th>
-                        <th className="bg-gray-100 px-2 py-1">Status</th>
-                        <th className="bg-gray-100 px-2 py-1">Started</th>
-                        <th className="bg-gray-100 px-2 py-1">Finished</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody className="[&>*:nth-child(odd)]:bg-gray-200 [&>*:nth-child(even)]:bg-gray-100">
-                    {
-                        books.map(book => {
-                            return (
-                                <tr key={book._id}>
-                                    <td className="px-2 py-1">
-                                        {editableBook === book._id ?
-                                            <input onInput={e => setInputTitle(e.target.value)} type="text" defaultValue={book.title} className="border border-gray-700 px-1 w-full"></input>
-                                            :
-                                            book.title
-                                        }
-                                    </td>
-                                    <td className="px-2 py-1">
-                                        {editableBook === book._id ?
-                                            <input onInput={e => setInputAuthor(e.target.value)} type="text" defaultValue={book.author} className="border border-gray-700 px-1 w-full"></input>
-                                            :
-                                            book.author
-                                        }
-                                    </td>
-                                    <td className={`px-2 py-1 font-semibold ${book.status === "Read" ? "text-green-600" : book.status === "Reading" ? "text-yellow-500" : "text-red-600"}`}>
-                                        {editableBook === book._id ?
-                                            <input onInput={e => setInputStatus(e.target.value)} type="text" defaultValue={book.status} className="border border-gray-700 px-1 w-full"></input>
-                                            :
-                                            book.status
-                                        }
-                                    </td>
-                                    <td className="px-2 py-1">
-                                        {editableBook === book._id ?
-                                            <input onInput={e => setInputStarted(e.target.value)} type="text" defaultValue={formatDateTime(book.started)} className="border border-gray-700 px-1 w-full"></input>
-                                            :
-                                            formatDateTime(book.started)
-                                        }
-                                    </td>
-                                    <td className="px-2 py-1">
-                                        {editableBook === book._id ?
-                                            <input onInput={e => setInputFinished(e.target.value)} type="text" defaultValue={formatDateTime(book.finished)} className="border border-gray-700 px-1 w-full"></input>
-                                            :
-                                            formatDateTime(book.finished)
-                                        }
-                                    </td>
-                                    <td className="font-bold px-2 bg-gray-900">
-                                        {editableBook === book._id ?
-                                            <>
-                                                <button className="text-green-700 pr-2" onClick={() => updateBook(book._id, inputTitle, inputAuthor, inputStatus, inputStarted, inputFinished)}>✓</button>
-                                                <button className="text-red-700" onClick={() => handleEdit('', '', '', '', '', '')}>X</button>
-                                            </>
-                                            :
-                                            <>
-                                                <button className="text-blue-700 pr-2" onClick={() => handleEdit(book._id, book.title, book.author, book.status, book.started, book.finished)}>EDIT</button>
-                                                <button className="text-red-700" onClick={() => deleteBook(book._id, book.title, book.author)}>DELETE</button>
-                                            </>
-                                        }
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                    {!editableBook ?
+            {loadingMessage ?
+                loadingMessage
+                :
+                <table className="text-black my-5 text-left border-cyan-700 border-l-8">
+                    <caption className="text-5xl text-white text-left font-thin my-10">Reading List</caption>
+                    <thead className="text-left">
                         <tr>
-                            <td className="px-2 py-1">
-                                <input onInput={e => setInputTitle(e.target.value)} type="text" placeholder="Book Title*" className={`px-1 w-full border ${errorMessages.includes(titleError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
-                            </td>
-                            <td className="px-2 py-1">
-                                <input onInput={e => setInputAuthor(e.target.value)} type="text" placeholder="Book Author*" className={`px-1 w-full border ${errorMessages.includes(authorError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
-                            </td>
-                            <td className="px-2 py-1">
-                                <input onInput={e => setInputStatus(e.target.value)} type="text" placeholder="Book Status" className={`px-1 w-full border ${errorMessages.includes(statusError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
-                            </td>
-                            <td className="px-2 py-1">
-                                <input onInput={e => setInputStarted(e.target.value)} type="text" placeholder="Started Date" className={`px-1 w-full border ${errorMessages.includes(startedError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
-                            </td>
-                            <td className="px-2 py-1">
-                                <input onInput={e => setInputFinished(e.target.value)} type="text" placeholder="Finished Date" className={`px-1 w-full border ${errorMessages.includes(finishedError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
-                            </td>
-                            <td className="bg-gray-900">
-                                <button onClick={() => addBook(inputTitle, inputAuthor, inputStatus, inputStarted, inputFinished)} className="text-yellow-500 font-bold px-2">ADD</button>
-                            </td>
+                            <th className="bg-gray-100 px-2 py-1">Title</th>
+                            <th className="bg-gray-100 px-2 py-1">Author</th>
+                            <th className="bg-gray-100 px-2 py-1">Status</th>
+                            <th className="bg-gray-100 px-2 py-1">Started</th>
+                            <th className="bg-gray-100 px-2 py-1">Finished</th>
+                            <th></th>
                         </tr>
-                        :
-                        <></>
-                    }
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="[&>*:nth-child(odd)]:bg-gray-200 [&>*:nth-child(even)]:bg-gray-100">
+                        {
+                            books.map(book => {
+                                return (
+                                    <tr key={book._id}>
+                                        <td className="px-2 py-1">
+                                            {editableBook === book._id ?
+                                                <input onInput={e => setInputTitle(e.target.value)} type="text" defaultValue={book.title} className="border border-gray-700 px-1 w-full"></input>
+                                                :
+                                                book.title
+                                            }
+                                        </td>
+                                        <td className="px-2 py-1">
+                                            {editableBook === book._id ?
+                                                <input onInput={e => setInputAuthor(e.target.value)} type="text" defaultValue={book.author} className="border border-gray-700 px-1 w-full"></input>
+                                                :
+                                                book.author
+                                            }
+                                        </td>
+                                        <td className={`px-2 py-1 font-semibold ${book.status === "Read" ? "text-green-600" : book.status === "Reading" ? "text-yellow-500" : "text-red-600"}`}>
+                                            {editableBook === book._id ?
+                                                <input onInput={e => setInputStatus(e.target.value)} type="text" defaultValue={book.status} className="border border-gray-700 px-1 w-full"></input>
+                                                :
+                                                book.status
+                                            }
+                                        </td>
+                                        <td className="px-2 py-1">
+                                            {editableBook === book._id ?
+                                                <input onInput={e => setInputStarted(e.target.value)} type="text" defaultValue={formatDateTime(book.started)} className="border border-gray-700 px-1 w-full"></input>
+                                                :
+                                                formatDateTime(book.started)
+                                            }
+                                        </td>
+                                        <td className="px-2 py-1">
+                                            {editableBook === book._id ?
+                                                <input onInput={e => setInputFinished(e.target.value)} type="text" defaultValue={formatDateTime(book.finished)} className="border border-gray-700 px-1 w-full"></input>
+                                                :
+                                                formatDateTime(book.finished)
+                                            }
+                                        </td>
+                                        <td className="font-bold px-2 bg-gray-900">
+                                            {editableBook === book._id ?
+                                                <>
+                                                    <button className="text-green-700 pr-2" onClick={() => updateBook(book._id, inputTitle, inputAuthor, inputStatus, inputStarted, inputFinished)}>✓</button>
+                                                    <button className="text-red-700" onClick={() => handleEdit('', '', '', '', '', '')}>X</button>
+                                                </>
+                                                :
+                                                <>
+                                                    <button className="text-blue-700 pr-2" onClick={() => handleEdit(book._id, book.title, book.author, book.status, book.started, book.finished)}>EDIT</button>
+                                                    <button className="text-red-700" onClick={() => deleteBook(book._id, book.title, book.author)}>DELETE</button>
+                                                </>
+                                            }
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        {!editableBook ?
+                            <tr>
+                                <td className="px-2 py-1">
+                                    <input onInput={e => setInputTitle(e.target.value)} type="text" placeholder="Book Title*" className={`px-1 w-full border ${errorMessages.includes(titleError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
+                                </td>
+                                <td className="px-2 py-1">
+                                    <input onInput={e => setInputAuthor(e.target.value)} type="text" placeholder="Book Author*" className={`px-1 w-full border ${errorMessages.includes(authorError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
+                                </td>
+                                <td className="px-2 py-1">
+                                    <input onInput={e => setInputStatus(e.target.value)} type="text" placeholder="Book Status" className={`px-1 w-full border ${errorMessages.includes(statusError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
+                                </td>
+                                <td className="px-2 py-1">
+                                    <input onInput={e => setInputStarted(e.target.value)} type="text" placeholder="Started Date" className={`px-1 w-full border ${errorMessages.includes(startedError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
+                                </td>
+                                <td className="px-2 py-1">
+                                    <input onInput={e => setInputFinished(e.target.value)} type="text" placeholder="Finished Date" className={`px-1 w-full border ${errorMessages.includes(finishedError) ? "outline outline-2 outline-red-700" : "border-gray-700"}`}></input>
+                                </td>
+                                <td className="bg-gray-900">
+                                    <button onClick={() => addBook(inputTitle, inputAuthor, inputStatus, inputStarted, inputFinished)} className="text-yellow-500 font-bold px-2">ADD</button>
+                                </td>
+                            </tr>
+                            :
+                            <></>
+                        }
+                    </tbody>
+                </table>
+            }
             <Link href="/endpoints" className="text-purple-400 underline block w-fit my-3">API Endpoints</Link>
             {
                 errorMessages.map(message => {
